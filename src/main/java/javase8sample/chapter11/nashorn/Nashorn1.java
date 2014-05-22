dae4e0cf694566d46955173ea6a43fe1728b9965
benhaile <denghaier@163.com>
2014-05-22 09:19:38 +0800

    Signed-off-by: benhaile <denghaier@163.com>

--- src/main/java/javase8sample/chapter11/nashorn/Nashorn1.java
+++ src/main/java/javase8sample/chapter11/nashorn/Nashorn1.java
@@ -1,19 +1,28 @@
 package javase8sample.chapter11.nashorn;
 
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.io.FileReader;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * Created by benhail on 2014/5/21.
  */
 public class Nashorn1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-        try {
-            engine.eval("print('Hello World!');");
-        } catch (ScriptException e) {
-            e.printStackTrace();
-        }
+        engine.eval(new FileReader("resources/nashorn1.js"));
+
+        Invocable invocable = (Invocable) engine;
+        Object result = invocable.invokeFunction("fun1", "Peter Parker");
+        System.out.println(result);
+        System.out.println(result.getClass());
+
+        invocable.invokeFunction("fun2", new Date());
+        invocable.invokeFunction("fun2", LocalDateTime.now());
+//        invocable.invokeFunction("fun2", new Person());
     }
 }
